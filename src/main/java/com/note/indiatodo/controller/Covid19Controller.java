9abf0036028c7093e20509742bd18a8f078ed2c8
adityabehera23@gmail.com <adityabehera23@gmail.com>
2021-05-23 12:47:13 +0530

    added covid19 vaccine tables and provide the limitation of the api call.

--- src/main/java/com/note/indiatodo/controller/Covid19Controller.java
+++ src/main/java/com/note/indiatodo/controller/Covid19Controller.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
 import com.note.indiatodo.dto.VaccineDetails;
+import io.github.bucket4j.Bucket;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,9 +22,23 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 
+/**
+ * headers = {
+ *     "accept":"application/json",
+ *     "Accept-Language": "hi_IN",
+ *     "cache-control":"max-age=0",
+ *     "sec-fetch-dest": "document",
+ *     "sec-fetch-mode": "navigate",
+ *     "upgrade-insecure-requests": "1",
+ *     "user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+ * }
+ */
 @Controller
 @RequestMapping("/")
 public class Covid19Controller {
+    @Autowired
+    private WelcomeController wc;
+
     @RequestMapping("/covidgo2")
     public ModelAndView getDistricts(HttpServletRequest req) throws IOException, InterruptedException {
         String state_id = req.getParameter("state_id");
@@ -50,26 +66,30 @@ public class Covid19Controller {
     }
     @RequestMapping("/covidVaccine")
     public ModelAndView getVaccines(HttpServletRequest req) throws IOException, InterruptedException {
-        final String dist_id = req.getParameter("dist_id");
-        final String date = req.getParameter("date");
-        String[] split = date.split("-");
-        String date2=split[2]+"-"+split[1]+"-"+split[0];
-        var url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id="+dist_id+"&date="+date2;
-        var request= HttpRequest.newBuilder().GET().uri(URI.create(url))
-                .headers("content-type","application/json","user-agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36","Accept-Language","en_US")
-                .build();
-        var client= HttpClient.newBuilder().build();
-        var response= client.send(request, HttpResponse.BodyHandlers.ofString());
-        JSONObject js= new JSONObject(response.body());
-        JSONArray distArr=js.getJSONArray("sessions");
-        final ObjectMapper objectMapper= new ObjectMapper();
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
-        List<VaccineDetails> vaccineDetails = objectMapper.readValue(distArr.toString(), new TypeReference<List<VaccineDetails>>() {
-        });
-        ModelAndView mv= new ModelAndView();
-        mv.setViewName("VaccineTable");
-        mv.addObject("vaccineList",vaccineDetails);
+        ModelAndView mv = new ModelAndView();
+        if(wc.getApiBucket().tryConsume(1)) {
+            final String dist_id = req.getParameter("dist_id");
+            final String date = req.getParameter("date");
+            String[] split = date.split("-");
+            String date2 = split[2] + "-" + split[1] + "-" + split[0];
+            var url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=" + dist_id + "&date=" + date2;
+            var request = HttpRequest.newBuilder().GET().uri(URI.create(url))
+                    .headers("content-type", "application/json", "user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36", "Accept-Language", "en_US")
+                    .build();
+            var client = HttpClient.newBuilder().build();
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject js = new JSONObject(response.body());
+            JSONArray distArr = js.getJSONArray("sessions");
+            final ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+            List<VaccineDetails> vaccineDetails = objectMapper.readValue(distArr.toString(), new TypeReference<List<VaccineDetails>>() {
+            });
+            mv.setViewName("VaccineTable");
+            mv.addObject("vaccineList", vaccineDetails);
+            return mv;
+        }
+        mv.setViewName("error");
         return mv;
     }
 }
--- src/main/java/com/note/indiatodo/controller/WelcomeController.java
+++ src/main/java/com/note/indiatodo/controller/WelcomeController.java
@@ -3,8 +3,13 @@ package com.note.indiatodo.controller;
 import com.note.indiatodo.dto.LoginUserValid;
 import com.note.indiatodo.dto.ModalConfig;
 import com.note.indiatodo.entity.Users;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,10 +19,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
 
 @Controller
 public class WelcomeController {
+
+    private final Bucket bucket;
+
+    public WelcomeController() {
+        Bandwidth b= Bandwidth.classic(100, Refill.greedy(100, Duration.ofMinutes(5)));
+        bucket= Bucket4j.builder().addLimit(b).build();
+    }
 
     @RequestMapping(value={"/welcome","/"})
     public ModelAndView welcomeUser(){
@@ -32,18 +45,25 @@ public class WelcomeController {
     }
     @RequestMapping("/covidgo")
     public ModelAndView covidRedirect() throws IOException, InterruptedException {
-        var url="https://cdn-api.co-vin.in/api/v2/admin/location/states";
-        var request= HttpRequest.newBuilder().GET().uri(URI.create(url))
-                .headers("content-type","application/json","user-agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36","Accept-Language","en_US")
-                .build();
-        var client= HttpClient.newBuilder().build();
-        var response= client.send(request, HttpResponse.BodyHandlers.ofString());
-        JSONObject js= new JSONObject(response.body());
-        JSONArray statesArr=js.getJSONArray("states");
-        ModelAndView mv= new ModelAndView();
-        mv.setViewName("CovidEntry");
-        mv.addObject("count",0);
-        mv.addObject("stateList",statesArr);
+        ModelAndView mv = new ModelAndView();
+        if(bucket.tryConsume(1)) {
+            var url = "https://cdn-api.co-vin.in/api/v2/admin/location/states";
+            var request = HttpRequest.newBuilder().GET().uri(URI.create(url))
+                    .headers("content-type", "application/json", "user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36", "Accept-Language", "en_US")
+                    .build();
+            var client = HttpClient.newBuilder().build();
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject js = new JSONObject(response.body());
+            JSONArray statesArr = js.getJSONArray("states");
+            mv.setViewName("CovidEntry");
+            mv.addObject("count", 0);
+            mv.addObject("stateList", statesArr);
+            return mv;
+        }
+        mv.setViewName("error");
         return mv;
     }
+        public Bucket getApiBucket(){
+            return this.bucket;
+        }
 }
